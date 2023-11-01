3842d297e812cf22415fbbaa9f1e9abb56596eef
Artem <Artem>
2023-11-01 10:27:50 +0200

    Fix for connection and prompting

--- index.py
+++ index.py
@@ -1,56 +1,15 @@
-
+import dotenv
+import pymysql
 import os
-import random
-from faker import Faker
 
-os.environ['OPENAI_API_KEY'] = ".........."
-import mysql.connector
-from langchain.agents import create_sql_agent
-from langchain.agents.agent_toolkits import SQLDatabaseToolkit
-from langchain.sql_database import SQLDatabase
-from langchain.llms.openai import OpenAI
-from datetime import datetime, timedelta
+from langchain.utilities import SQLDatabase
+from langchain.llms import OpenAI
+from langchain_experimental.sql import SQLDatabaseChain
 
-DATABASE = {
-    "host": os.getenv("DB_HOST"),
-    "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
-    "name": os.getenv("DB_NAME")
-}
-num_users = 100
+dotenv.load_dotenv()
 
-def generate_users():
-    fake = Faker()
-    conn = mysql.connector.connect(**DATABASE)
-    cursor = conn.cursor()
+db = SQLDatabase.from_uri("mysql://root:admin@127.0.0.1/poppins")
+llm = OpenAI(temperature=0, verbose=True)
+db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
 
-    for _ in range(num_users):
-        username = fake.user_name()
-        password = fake.password()
-        created_at = fake.date_time_between(start_date="-1 year", end_date="now")
-        last_login = created_at + timedelta(days=random.randint(1, 365))
-        purchases = random.randint(0, 10)
-        last_purchase = created_at + timedelta(days=random.randint(1, 365))
-    
-    query = "INSERT INTO users (username, password, created_at, last_login, purchases, last_purchase) VALUES (%s, %s, %s, %s, %s, %s)"
-    values = (username, password, created_at, last_login, purchases, last_purchase)
-    cursor.execute(query, values)
-    
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-
-# Connect to the database and execute the SQL script
-# TODO - make it create dummy data for SQL
-if os.getenv("GENERATE_DATA") == "True":
-
-# Create the agent executor
-db = SQLDatabase.from_uri("sqlite:///./Chinook.db")
-toolkit = SQLDatabaseToolkit(db=db)
-agent_executor = create_sql_agent(
-    llm=OpenAI(temperature=0),
-    toolkit=toolkit,
-    verbose=True
-)
-
+db_chain.run("How many employees are there?")
